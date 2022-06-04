4489bca1702f83f0b7042ce2fca5f7d314c912f6
ynrd <ynrd93@gmail.com>
2022-06-04 13:19:58 +0100

    Laboratorio Final Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Socio } from './socio';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mi-app';
+  socio: Socio = this.newSocio()
+  socios: Socio[] = []
+  opcionSeleccionada: string = " ";
+
+  getSocio(){
+    return JSON.stringify(this.socio);
+  }
+
+  addSocio(){
+    console.log("add a person");
+    this.socios.push({...this.socio})
+    this.socio = this.newSocio()
+  }
+  
+  onSubmit(){
+    console.log(this.socio)
+  }
+private newSocio(): Socio {
+  return {
+    nombre: '',
+    apellidos: '',
+    socioID: 0,
+    dni: "",
+    telefono: 0,
+    sexo: '',
+  }
 }
+
+seleccion(){
+  this.socio.sexo= this.opcionSeleccionada;
+}
+
+editSocio(){
+this.socio.nombre = String (this.getSocio());
+}
+
+deleteSocio(dni: string){
+  this.socios = this.socios.filter(socio => socio.dni !== dni);
+}
+
+}
+
---
+++ src/app/socio.ts
@@ -0,0 +1,9 @@
+export interface Socio {
+    nombre: string;
+    apellidos: string;
+    socioID: number;
+    dni: string;
+    telefono: number;
+    sexo: string;
+  }
+  
